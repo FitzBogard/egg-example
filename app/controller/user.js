3dcd0764e69aef2347babb9369222acd32ea87ed
fitzbogard <fitzbogard@qq.com>
2023-04-05 00:37:43 +0800

    1

---
+++ app/controller/user.js
@@ -0,0 +1,18 @@
+const Controller = require('egg').Controller;
+
+class UserController extends Controller {
+  async register() {
+    const { ctx } = this;
+    const { username, password } = ctx.request.body;
+    if (!username || !password) {
+      ctx.body = {
+        code: 500,
+        msg: '账号密码不能为空',
+        data: null,
+      };
+      return;
+    }
+  }
+}
+
+module.exports = UserController;
